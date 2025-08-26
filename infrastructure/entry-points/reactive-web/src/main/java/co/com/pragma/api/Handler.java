4f5f56770f9f6597cf79649b39b1f176b92d3a58
VictorRoe <victorroe819@gmail.com>
2025-08-26 15:13:16 -0500

    add: entry-points

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/Handler.java
@@ -1,29 +1,37 @@
 package co.com.pragma.api;
 
+import co.com.pragma.model.user.User;
+import co.com.pragma.usecase.user.UserUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @Component
 @RequiredArgsConstructor
 public class Handler {
-//private  final UseCase useCase;
-//private  final UseCase2 useCase2;
 
-    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
-        // useCase.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
-
-    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
-        // useCase2.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
+        private final UserUseCase userCase;
 
     public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
-        // useCase.logic();
-        return ServerResponse.ok().bodyValue("");
+        return serverRequest.bodyToMono(User.class)
+                .flatMap(userCase::registerUser)
+                .flatMap(savedUser -> ServerResponse
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(savedUser))
+                .onErrorResume(error -> ServerResponse
+                        .badRequest()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(
+                                Map.of(
+                                        "error", error.getClass().getSimpleName(),
+                                        "message" ,error.getMessage()
+                                )
+                        ));
     }
 }
