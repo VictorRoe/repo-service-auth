9aead3934afea485255df4481925a4607f6f9570
VictorRoe <victorroe819@gmail.com>
2025-08-28 17:25:24 -0500

    add: driven adapters r2dbc postgress driver

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/MyReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/MyReactiveRepositoryAdapter.java
@@ -1,23 +1,35 @@
 package co.com.pragma.r2dbc;
 
+import co.com.pragma.model.user.User;
+import co.com.pragma.model.user.gateways.UserRepository;
+import co.com.pragma.r2dbc.entity.UserEntity;
 import co.com.pragma.r2dbc.helper.ReactiveAdapterOperations;
 import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.data.relational.core.sql.TrueCondition;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.reactive.TransactionalOperator;
+import reactor.core.publisher.Mono;
 
 @Repository
 public class MyReactiveRepositoryAdapter extends ReactiveAdapterOperations<
-    Object/* change for domain model */,
-    Object/* change for adapter model */,
-    String,
-    MyReactiveRepository
-> {
-    public MyReactiveRepositoryAdapter(MyReactiveRepository repository, ObjectMapper mapper) {
-        /**
-         *  Could be use mapper.mapBuilder if your domain model implement builder pattern
-         *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
-         *  Or using mapper.map with the class of the object model
-         */
-        super(repository, mapper, d -> mapper.map(d, Object.class/* change for domain model */));
+        User,
+        UserEntity,
+        Long,
+        MyReactiveRepository
+> implements UserRepository {
+    private TransactionalOperator transactionalOperator; // chequear
+
+    public MyReactiveRepositoryAdapter(MyReactiveRepository repository, ObjectMapper mapper, TransactionalOperator transactionalOperator) {
+        super(repository, mapper, d -> mapper.map(d, User.class));
     }
 
+    @Override
+    public Mono<User> saveUser(User user) {
+        return this.save(user).as(transactionalOperator::transactional);
+    }
+
+    @Override
+    public Mono<Boolean> existsByEmail(String email) {
+        return repository.existsByEmail(email);
+    }
 }
---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/entity/UserEntity.java
@@ -0,0 +1,31 @@
+package co.com.pragma.r2dbc.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate birthDate;
+    private String address;
+    private String phoneNumber;
+    private Long baseSalary;
+}
