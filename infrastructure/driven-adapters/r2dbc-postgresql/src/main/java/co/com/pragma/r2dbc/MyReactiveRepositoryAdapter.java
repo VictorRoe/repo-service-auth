e47ebd77d4a3c18dddc9f1e47cc5f20033ab87af
VictorRoe <victorroe819@gmail.com>
2025-08-29 17:09:54 -0500

    fix: Fix problem about TransactionalOperator Class

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/MyReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/MyReactiveRepositoryAdapter.java
@@ -5,7 +5,6 @@ import co.com.pragma.model.user.gateways.UserRepository;
 import co.com.pragma.r2dbc.entity.UserEntity;
 import co.com.pragma.r2dbc.helper.ReactiveAdapterOperations;
 import org.reactivecommons.utils.ObjectMapper;
-import org.springframework.data.relational.core.sql.TrueCondition;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.reactive.TransactionalOperator;
 import reactor.core.publisher.Mono;
@@ -17,10 +16,11 @@ public class MyReactiveRepositoryAdapter extends ReactiveAdapterOperations<
         Long,
         MyReactiveRepository
 > implements UserRepository {
-    private TransactionalOperator transactionalOperator; // chequear
+    private  final TransactionalOperator transactionalOperator; // chequear
 
     public MyReactiveRepositoryAdapter(MyReactiveRepository repository, ObjectMapper mapper, TransactionalOperator transactionalOperator) {
         super(repository, mapper, d -> mapper.map(d, User.class));
+        this.transactionalOperator = transactionalOperator;
     }
 
     @Override
